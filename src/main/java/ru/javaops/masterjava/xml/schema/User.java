72f3c7350759f4ffd50deb8ba8f66d85c932f0ac
Stanislav <stasn@ua.fm>
2017-10-28 22:00:25 +0300

    Homework 1-4

--- src/main/java/ru/javaops/masterjava/xml/schema/User.java
+++ src/main/java/ru/javaops/masterjava/xml/schema/User.java
@@ -1,10 +1,15 @@
 
 package ru.javaops.masterjava.xml.schema;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+import javax.xml.bind.JAXBElement;
 import javax.xml.bind.annotation.XmlAccessType;
 import javax.xml.bind.annotation.XmlAccessorType;
 import javax.xml.bind.annotation.XmlAttribute;
 import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlElementRef;
 import javax.xml.bind.annotation.XmlIDREF;
 import javax.xml.bind.annotation.XmlRootElement;
 import javax.xml.bind.annotation.XmlSchemaType;
@@ -21,11 +26,12 @@ import javax.xml.bind.annotation.XmlType;
  *   &lt;complexContent>
  *     &lt;restriction base="{http://www.w3.org/2001/XMLSchema}anyType">
  *       &lt;sequence>
- *         &lt;element name="email" type="{http://www.w3.org/2001/XMLSchema}string"/>
  *         &lt;element name="fullName" type="{http://www.w3.org/2001/XMLSchema}string"/>
+ *         &lt;element name="group" type="{http://www.w3.org/2001/XMLSchema}IDREF" maxOccurs="unbounded" minOccurs="0"/>
  *       &lt;/sequence>
  *       &lt;attribute name="flag" use="required" type="{http://javaops.ru}flagType" />
  *       &lt;attribute name="city" use="required" type="{http://www.w3.org/2001/XMLSchema}IDREF" />
+ *       &lt;attribute name="email" use="required" type="{http://www.w3.org/2001/XMLSchema}string" />
  *     &lt;/restriction>
  *   &lt;/complexContent>
  * &lt;/complexType>
@@ -35,46 +41,24 @@ import javax.xml.bind.annotation.XmlType;
  */
 @XmlAccessorType(XmlAccessType.FIELD)
 @XmlType(name = "", propOrder = {
-    "email",
-    "fullName"
+    "fullName",
+    "group"
 })
 @XmlRootElement(name = "User", namespace = "http://javaops.ru")
 public class User {
 
     @XmlElement(namespace = "http://javaops.ru", required = true)
-    protected String email;
-    @XmlElement(namespace = "http://javaops.ru", required = true)
     protected String fullName;
+    @XmlElementRef(name = "group", namespace = "http://javaops.ru", type = JAXBElement.class, required = false)
+    protected List<JAXBElement<Object>> group;
     @XmlAttribute(name = "flag", required = true)
     protected FlagType flag;
     @XmlAttribute(name = "city", required = true)
     @XmlIDREF
     @XmlSchemaType(name = "IDREF")
     protected Object city;
-
-    /**
-     * Gets the value of the email property.
-     * 
-     * @return
-     *     possible object is
-     *     {@link String }
-     *     
-     */
-    public String getEmail() {
-        return email;
-    }
-
-    /**
-     * Sets the value of the email property.
-     * 
-     * @param value
-     *     allowed object is
-     *     {@link String }
-     *     
-     */
-    public void setEmail(String value) {
-        this.email = value;
-    }
+    @XmlAttribute(name = "email", required = true)
+    protected String email;
 
     /**
      * Gets the value of the fullName property.
@@ -98,6 +82,35 @@ public class User {
      */
     public void setFullName(String value) {
         this.fullName = value;
+    }
+
+    /**
+     * Gets the value of the group property.
+     * 
+     * <p>
+     * This accessor method returns a reference to the live list,
+     * not a snapshot. Therefore any modification you make to the
+     * returned list will be present inside the JAXB object.
+     * This is why there is not a <CODE>set</CODE> method for the group property.
+     * 
+     * <p>
+     * For example, to add a new item, do as follows:
+     * <pre>
+     *    getGroup().add(newItem);
+     * </pre>
+     * 
+     * 
+     * <p>
+     * Objects of the following type(s) are allowed in the list
+     * {@link JAXBElement }{@code <}{@link Object }{@code >}
+     * 
+     * 
+     */
+    public List<JAXBElement<Object>> getGroup() {
+        if (group == null) {
+            group = new ArrayList<JAXBElement<Object>>();
+        }
+        return this.group;
     }
 
     /**
@@ -148,4 +161,61 @@ public class User {
         this.city = value;
     }
 
+    /**
+     * Gets the value of the email property.
+     * 
+     * @return
+     *     possible object is
+     *     {@link String }
+     *     
+     */
+    public String getEmail() {
+        return email;
+    }
+
+    /**
+     * Sets the value of the email property.
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link String }
+     *     
+     */
+    public void setEmail(String value) {
+        this.email = value;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof User)) return false;
+
+        User user = (User) o;
+
+        return email != null ? email.equals(user.email) : user.email == null;
+    }
+
+    @Override
+    public int hashCode() {
+        return email != null ? email.hashCode() : 0;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "fullName='" + fullName + '\'' +
+                ", group=" + groupsToGroupsId()+
+                ", flag=" + flag +
+                ", city=" + cityToString() +
+                ", email='" + email + '\'' +
+                '}';
+    }
+
+    public List<String> groupsToGroupsId(){
+        return group.stream().map(x->((GroupSimple)x.getValue()).getId()).collect(Collectors.toList());
+    }
+    public String cityToString(){
+        return ((CityType)city).getValue();
+    }
 }
+
---
+++ src/test/java/ru/javaops/masterjava/MainXML.java
@@ -0,0 +1,85 @@
+package ru.javaops.masterjava;
+
+import com.google.common.io.Resources;
+import org.w3c.dom.NodeList;
+import ru.javaops.masterjava.xml.schema.*;
+import ru.javaops.masterjava.xml.util.JaxbParser;
+import ru.javaops.masterjava.xml.util.Schemas;
+import ru.javaops.masterjava.xml.util.XPathProcessor;
+
+import javax.xml.bind.JAXBException;
+import javax.xml.xpath.XPathConstants;
+import javax.xml.xpath.XPathExpression;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.*;
+
+import java.util.stream.IntStream;
+import static java.util.stream.Collectors.toList;
+
+
+public class MainXML {
+
+    public static void main(String[] args) throws Exception {
+        System.out.println(getUsersByProject("topjava"));
+        System.out.println(getUsersByProjectWithXPath("topjava"));
+    }
+
+    private static final JaxbParser JAXB_PARSER = new JaxbParser(ObjectFactory.class);
+
+    static {
+        JAXB_PARSER.setSchema(Schemas.ofClasspath("payload.xsd"));
+    }
+
+    public static Collection<User> getUsersByProject(String projectName) throws IOException, JAXBException {
+        Payload payload = JAXB_PARSER.unmarshal(
+                Resources.getResource("payload.xml").openStream());
+        List<GroupSimple> groupSimples = payload.getProjects().getProject()
+                .stream().filter(x -> x.getName().equals(projectName)).findFirst()
+                .get().getGroup();
+        List<User> result = new ArrayList<>();
+        for (GroupSimple group : groupSimples) {
+            for (User user : payload.getUsers().getUser()) {
+                boolean isPresent = user.getGroup().stream().anyMatch(groupUser -> ((GroupSimple) groupUser.getValue()).getId().equals(group.getId()));
+                if (isPresent) {
+                    result.add(user);
+                }
+            }
+        }
+
+        return result.stream().distinct().sorted(Comparator.comparing(User::getFullName)).collect(toList());
+    }
+
+    public static List<String> getUsersByProjectWithXPath(String projectName) throws IOException {
+        try (InputStream is =
+                     Resources.getResource("payload.xml").openStream()) {
+            XPathProcessor processor = new XPathProcessor(is);
+            XPathExpression expression =
+                    XPathProcessor.getExpression(
+                            "/*[name()='Payload']/*[name()='Projects']/*[name()='Project'][@name='" +
+                                    projectName + "']/*[name()='Group']");
+            NodeList nodes = processor.evaluate(expression, XPathConstants.NODESET);
+            List<String> groupsName = new ArrayList<>();
+            //First fet the groups
+            IntStream.range(0, nodes.getLength()).forEach(
+                    i -> groupsName.add(nodes.item(i).getAttributes().getNamedItem("id").getNodeValue()));
+
+            expression = XPathProcessor.getExpression("/*[name()='Payload']/*[name()='Users']/*[name()='User']");
+            NodeList users = processor.evaluate(expression, XPathConstants.NODESET);
+
+            // Second find Users with that Group
+            List<String> result = new ArrayList<>();
+            IntStream.range(0, users.getLength()).forEach(i ->
+                    groupsName.forEach(group -> {
+                        String resultString = "";
+                        if (users.item(i).getTextContent().contains(group)) {
+                            String[] str = users.item(i).getTextContent().trim().split("\n");
+                            resultString += str[0].trim();
+                            resultString += "\t" + users.item(i).getAttributes().getNamedItem("email");
+                            result.add(resultString);
+                        }
+                    }));
+            return result.stream().distinct().sorted(Comparator.comparing(String::toString)).collect(toList());
+        }
+    }
+}
