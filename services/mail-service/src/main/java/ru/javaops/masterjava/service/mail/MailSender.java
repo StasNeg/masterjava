75b1c03cc0e1244ef009fd4a2d615c06a2a15b7d
Stanislav <stasn@ua.fm>
2017-12-09 19:28:36 +0200

    8 5 soap exceptions

--- services/mail-service/src/main/java/ru/javaops/masterjava/service/mail/MailSender.java
+++ services/mail-service/src/main/java/ru/javaops/masterjava/service/mail/MailSender.java
@@ -8,6 +8,7 @@ import org.apache.commons.mail.EmailException;
 import ru.javaops.masterjava.persist.model.DBIProvider;
 import ru.javaops.masterjava.service.mail.persist.MailCase;
 import ru.javaops.masterjava.service.mail.persist.MailCaseDao;
+import ru.javaops.web.WebStateException;
 
 import java.util.Set;
 
@@ -15,12 +16,12 @@ import java.util.Set;
 public class MailSender {
     private static final MailCaseDao MAIL_CASE_DAO = DBIProvider.getDao(MailCaseDao.class);
 
-    static MailResult sendTo(Addressee to, String subject, String body) {
+    static MailResult sendTo(Addressee to, String subject, String body) throws WebStateException {
         val state = sendToGroup(ImmutableSet.of(to), ImmutableSet.of(), subject, body);
         return new MailResult(to.getEmail(), state);
     }
 
-    static String sendToGroup(Set<Addressee> to, Set<Addressee> cc, String subject, String body) {
+    static String sendToGroup(Set<Addressee> to, Set<Addressee> cc, String subject, String body) throws WebStateException {
         log.info("Send mail to \'" + to + "\' cc \'" + cc + "\' subject \'" + subject + (log.isDebugEnabled() ? "\nbody=" + body : ""));
         String state = MailResult.OK;
         try {
@@ -42,7 +43,12 @@ public class MailSender {
             log.error(e.getMessage(), e);
             state = e.getMessage();
         }
-        MAIL_CASE_DAO.insert(MailCase.of(to, cc, subject, state));
+        try {
+            MAIL_CASE_DAO.insert(MailCase.of(to, cc, subject, state));
+        } catch (Exception e) {
+            log.error("Mail history saving exception", e);
+            throw new WebStateException(e, ExceptionType.DATA_BASE);
+        }
         log.info("Sent with state: " + state);
         return state;
     }
